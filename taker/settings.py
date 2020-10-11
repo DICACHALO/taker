48132ef722e066d06186429ce914fd8dadcbfc26
Diana Carolina Chacón López <dchacon871@unab.edu.co>
2020-10-11 18:36:03 -0500

    debug true

--- taker/settings.py
+++ taker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wz#p$g_d3@#=$#wpi^c-+pl+o5%7&nzwjix^kb$l!l%&*sa@$q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
