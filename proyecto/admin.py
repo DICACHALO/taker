64fb47123636542fa6cfc42c28fc94bcf403f256
Diana Carolina Chacón López <dchacon871@unab.edu.co>
2020-10-20 11:50:37 -0500

    prueba 1: eliminando carpetas del sistema

--- proyecto/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Project, User_Project
-
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(User_Project)
--- specification/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpecificationConfig(AppConfig):
-    name = 'specification'
